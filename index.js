ad890f8e643d80bdecbcd268ba974e1199a50e08
Sweetlight95 <princessadesuwa93@gmail.com>
2021-11-10 14:34:40 +0100

    great

---
+++ index.js
@@ -0,0 +1,66 @@
+// console.log(document.getElementsByTagName("Body")[0].innerHTML)
+document.addEventListener("DOMContentLoaded", function () {
+
+    let registerObject ={}
+    // let repository = [
+    //     {first_name: "Rebecca", last_name: "Odoemela", phone_number: "08152728792", password:"2733"},
+    //     {first_name: "ujay", last_name: "Eronmol", phone_number: "09876775554", password:"745643"},
+    // ]
+    function confirmRegistration() {
+        if (Object.keys(registerObject).length === 4){
+            try{
+                let userRepository = LocalRepository.getItem("ewauser")
+                if (userRepository === null){
+                    let repository = [registerObject]
+                    localStorage.setItem("ewauser", JSON.stringify(repository))
+                } else {
+                    let previousRepository = JSON.parse(userRepository)
+                    let updatedRepository = [...previousRepository, registerObject]
+                    localStorage.setItem("ewauser", JSON.stringify(updatedRepository))
+                }
+            } catch(error) {
+                console.log(error)
+            }
+        } else {
+            alert("Incomplete User Info")
+        }
+    }
+    let button = document.getElementsByClassName("register_btn")[0]
+    button.addEventListener("click", () => confirmRegistration())
+
+
+    function handleInputChange(e) {
+    registerObject = {...registerObject, [e.target.name]: e.target.value}
+}
+
+    let inputFields = document.getElementsByTagName('input')
+
+    Array.from(inputFields).forEach((inputField) => 
+    inputField.addEventListener("input", (e) => handleInputChange(e)))
+
+    function comfirmLogin(){
+        let {first_name, password} = registerObject
+
+            if(repository.findIndex(user => user.first_name === first_name) !== -1){
+
+            if(repository.find(user => user.first_name === first_name).password === password){
+                localStorage.setItem("ewauser", JSON.stringify(registerObject))
+                location.href = 'home.html'
+                // window.location.href = etc
+            }else {
+                alert('Please enter a correct password')
+            }
+        } else {
+            alert('User Does Not Exist')
+        }
+    }
+})
+
+
+    // for(let i = 0; i < inputFields.length; i++) {
+    //     inputFields[i].addEventListener("input", (e) => handleInputChange(e))
+    // }
+// OR
+    // function handleInputChange(e) {
+    //     console.log(e)
+    // }
